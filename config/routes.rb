c6c6eb0434a4a75278650ebe2544cc33ce756102
StefanoM99 <stefano.mazziottaa@gmail.com>
2022-08-31 11:15:10 +0200

    Inserita pagina di registrazione+ migration utenti

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get "/", to: "home#index"
-  root "application#home"
+  get '/registration',to:"user#registration"
+  get "/home", to: "home#index"
+  root "home#index"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
