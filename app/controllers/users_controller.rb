fdae097fabb3d2bd3ca08161674886792622a098
Luca Di Matteo <dimatteoluca1995@gmail.com>
2022-10-25 15:01:24 +0200

    Add follow/unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -78,6 +78,18 @@ class UsersController < ApplicationController
     end
 end
 
+def follow
+  @user = User.find(params[:id])
+  current_user.followees << @user
+  redirect_back(fallback_location: user_path(@user))
+end
+
+def unfollow
+  @user = User.find(params[:id])
+  current_user.followed_users.find_by(followee_id: @user.id).destroy
+  redirect_back(fallback_location: user_path(@user))
+end
+
   # DELETE /profiles/1 or /profiles/1.json
   def destroy
     @user.destroy
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_23_144329) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_25_113312) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -81,6 +81,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_23_144329) do
   end
 
   create_table "feeds", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followee_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
