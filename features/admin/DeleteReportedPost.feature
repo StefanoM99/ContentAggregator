0cd2190e0b9a87ecfeec064a7e4b24c71a7841ae
Luca Di Matteo <dimatteoluca1995@gmail.com>
2022-10-30 17:55:13 +0100

    Add all DeleteReportedPost feature's tests

--- features/admin/DeleteReportedPost.feature
+++ features/admin/DeleteReportedPost.feature
@@ -4,6 +4,7 @@ Feature: Admin deletes a reported post
     Background:
         Given I signed in as admin
 
+    @single
     Scenario: Admin deletes 1 in 1 reported posts
         Given there is 1 reported post by "Autore DelPost" with title "Titolo" and summary "Sommario"
         And I am on reported-posts page
@@ -17,6 +18,7 @@ Feature: Admin deletes a reported post
         And I should not see "Titolo"
         And I should not see "Sommario"
 
+    @single
     Scenario: Admin deletes 1 in 3 reported posts by Autore DelPost
         Given there are 3 reported posts by "Autore DelPost"
         And I am on reported-posts page
@@ -33,6 +35,7 @@ Feature: Admin deletes a reported post
         And I should see "Titolo 3 post"
         And I should see 2 "Elimina"
 
+    @single
     Scenario: Admin deletes 1 in 10 reported posts by Autore DelPost
         Given there are 10 reported posts by "Autore DelPost"
         And I am on reported-posts page
@@ -45,17 +48,14 @@ Feature: Admin deletes a reported post
         And I should not see "Titolo 1 post"
         And I should see 9 "Elimina"
 
-    # @prova
-    # Scenario: Admin deletes 1 in 1 reported posts
-    #     Given there is 1 reported post by "Autore DelPost" with title "Titolo" and summary "Sommario"
-    #     And there is 1 reported post by "Autore DelPost" with title "Titolo" and summary "Sommario"
-    #     And I am on reported-posts page
-    #     Then I should see "Autore DelPost"
-    #     And I should see "Titolo"
-    #     And I should see "Sommario"
-    #     And I should see "Elimina"
-    #     When I press "Elimina"
-    #     Then I should be on reported-posts page
-    #     And  I should not see "Autore DelPost"
-    #     And I should not see "Titolo"
-    #     And I should not see "Sommario"
+    @many
+    Scenario: Admin deletes 1 Autore DelPost's reported post and 9 reported posts
+        Given there is 1 reported post by "Autore DelPost" and 9 other reported posts
+        And I am on reported-posts page
+        Then I should see 1 "Autore DelPost"
+        And I should see 10 "Elimina"
+        When I press first "Elimina"
+        Then I should be on reported-posts page
+        And I should not see "Autore DelPost"
+        And I should see 9 "Elimina"
+
--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -1,15 +1,31 @@
-Given('I signed up as "prova@gmail.com"') do
-    @user = User.new(:name => 'Test', :surname => 'Prova', :email => 'prova@gmail.com', :password => 'password', :password_confirmation => 'password')
-    @user.save
-end
+# Given('I signed up as "prova@gmail.com"') do
+#     @user = User.new(:name => 'Test', :surname => 'Prova', :email => 'prova@gmail.com', :password => 'password', :password_confirmation => 'password')
+#     @user.save
+# end
 
-Given('I signed in as "prova@gmail.com"') do
-    @user = User.new(:name => 'Test', :surname => 'Prova', :email => 'prova@gmail.com', :password => 'password', :password_confirmation => 'password')
-    @user.save
-    visit path_to("home page")
-    fill_in('E-Mail', :with => @user.email)
-    fill_in('Password', :with => @user.password)
-    click_button('Accedi')
+# Given('I signed in as "prova@gmail.com"') do
+#     @user = User.new(:name => 'Test', :surname => 'Prova', :email => 'prova@gmail.com', :password => 'password', :password_confirmation => 'password')
+#     @user.save
+#     visit path_to("home page")
+#     fill_in('E-Mail', :with => @user.email)
+#     fill_in('Password', :with => @user.password)
+#     click_button('Accedi')
+# end
+
+Given('I signed in as {string}') do |email|
+    begin
+        if (User.find_by email: (email)) != {}
+            (User.find_by email: (email)).destroy
+        end
+    rescue
+    ensure
+        @user = User.new(:name => 'Test', :surname => 'Prova', :email => email, :password => 'password', :password_confirmation => 'password')
+        @user.save
+        visit path_to("home page")
+        fill_in('E-Mail', :with => @user.email)
+        fill_in('Password', :with => @user.password)
+        click_button('Accedi')
+    end
 end
 
 # Given('I signed in as admin') do
@@ -35,10 +51,6 @@ Given('I signed in as admin') do
         fill_in('Password', :with => @user.password)
         click_button('Accedi')
     end
-end
-
-Given('there are reported posts') do
-    @reported_post = ReportedPost.all[0]
 end
 
 # Given('there is 1 reported post by "Autore DelPost" with title "Titolo" and summary "Sommario"') do
@@ -113,21 +125,28 @@ end
 #     end
 # end
 
-Given('there are {int} reported posts by {string}') do |int, string|
+Given('there are {int} reported posts by {string}') do |int, full_name|
     if (ReportedPost.all != {})
         ReportedPost.all.each do |rep_post|
             rep_post.destroy
         end
     end
-    name = string.split(" ")[0]
-    surname = string.split(" ")[1]
-    @author = User.new(:name => name, :surname => surname, :email => name+surname+'@gmail.com', :password => 'password', :password_confirmation => 'password')
-    @author.save
-    for i in 1..int do
-        @post = Post.new(author: @author.name+" "+@author.surname, title: 'Titolo '+(i).to_s+' post ', summary: 'Sommario', post_file: nil, user_id: @author.id)
-        @post.save
-        @reported_post = ReportedPost.new(author: 'Autore DelPost', title: 'Titolo '+(i).to_s+' post ', summary: 'Sommario', post_file: nil, user_id: 1, post_id: @post.id)
-        @reported_post.save
+    name = full_name.split(" ")[0]
+    surname = full_name.split(" ")[1]
+    begin
+        if (User.find_by email: (name+surname+'@gmail.com')) != {}
+            (User.find_by email: (name+surname+'@gmail.com')).destroy
+        end
+    rescue
+    ensure
+        @author = User.new(:name => name, :surname => surname, :email => name+surname+'@gmail.com', :password => 'password', :password_confirmation => 'password')
+        @author.save
+        for i in 1..int do
+            @post = Post.new(author: @author.name+" "+@author.surname, title: 'Titolo '+(i).to_s+' post ', summary: 'Sommario', post_file: nil, user_id: @author.id)
+            @post.save
+            @reported_post = ReportedPost.new(author: @author.name+" "+@author.surname, title: 'Titolo '+(i).to_s+' post ', summary: 'Sommario', post_file: nil, user_id: 1, post_id: @post.id)
+            @reported_post.save
+        end
     end
 end
 
@@ -149,16 +168,6 @@ Then('I should see the image I uploaded') do
     page.find(class: 'articleimg')['src'].end_with?("logo.png")
 end
 
-When('I upload a video') do
-    page.attach_file('app/assets/videos/video.mp4') do
-        page.find('#post_post_file').click
-    end
-end
-
-Then('I should see the video I uploaded') do
-    page.find('video')['src'].end_with?("video.mp4")
-end
-
 When('I upload a text file') do
     page.attach_file('app/assets/file.txt') do
         page.find('#post_post_file').click
@@ -171,4 +180,42 @@ end
 
 When(/^(?:|I )press first "([^"]*)"$/) do |button|
     first(:button, button).click
+end
+
+Given('there is 1 reported post by {string} and 9 other reported posts') do |full_name|
+    if (ReportedPost.all != {})
+        ReportedPost.all.each do |rep_post|
+            rep_post.destroy
+        end
+    end
+    name = full_name.split(" ")[0]
+    surname = full_name.split(" ")[1]
+    begin
+        if (User.find_by email: (name+surname+'@gmail.com')) != {}
+            (User.find_by email: (name+surname+'@gmail.com')).destroy
+        end
+    rescue
+    ensure
+        @author = User.new(:name => name, :surname => surname, :email => name+surname+'@gmail.com', :password => 'password', :password_confirmation => 'password')
+        @author.save
+        @post = Post.new(author: @author.name+" "+@author.surname, title: "Titolo", summary: "Sommario", post_file: nil, user_id: @author.id)
+        @post.save
+        @reported_post = ReportedPost.new(author: @author.name+" "+@author.surname, title: "Titolo", summary: "Sommario", post_file: nil, user_id: 1, post_id: @post.id)
+        @reported_post.save
+    end
+    begin
+        if (User.find_by email: ("Altro"+"Autore"+'@gmail.com')) != {}
+            (User.find_by email: ("Altro"+"Autore"+'@gmail.com')).destroy
+        end
+    rescue
+    ensure
+        @author2 = User.new(:name => "Altro", :surname => "Autore", :email => "Altro"+"Autore"+'@gmail.com', :password => 'password', :password_confirmation => 'password')
+        @author2.save
+        for i in 1..9 do
+            @post2 = Post.new(author: @author2.name+" "+@author2.surname, title: 'Titolo '+(i).to_s+' post ', summary: 'Sommario', post_file: nil, user_id: @author2.id)
+            @post2.save
+            @reported_post2 = ReportedPost.new(author: @author2.name+" "+@author2.surname, title: 'Titolo '+(i).to_s+' post ', summary: 'Sommario', post_file: nil, user_id: 1, post_id: @post2.id)
+            @reported_post2.save
+        end
+    end
 end
